443983a5aa60dac307f7936c3d4ef30471ce2da4
Cezary Olejarczyk <colejarczyk@divante.pl>
2017-10-05 09:42:58 +0200

    API DOC

--- conf.py
+++ conf.py
@@ -10,6 +10,7 @@ project = 'Open Loyalty'
 copyright = u'2011-2017, Divante Ltd'
 version = ''
 release = ''
+html_logo = "_images/logo.png"
 html_theme = 'ol_rtd_theme'
 html_theme_path = ["_themes"]
 htmlhelp_basename = 'Openloyaltydocs'
